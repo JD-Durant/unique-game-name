9fc8c5e042997252b583fa6473912f484ffb29dc
IProDurant <joshdurant2001@gmail.com>
2020-09-18 10:56:53 +0100

    2 files modified

--- Source/Scenes/game.js
+++ Source/Scenes/game.js
@@ -5,5 +5,10 @@ export class actualGame extends Phaser.Scene {constructor(){super("game");}
     this.scene.launch("keyDetector") //launches a scene in the background, this is how i will carry out any key detection
 
     var grid = this.add.image(950, 455, "gameGrid").setScale(0.85);
+
+    var note1 = this.add.image(798, 100, "note1").setScale(0.4);
+    var note2 = this.add.image(898, 100, "note2").setScale(0.4);
+    var note3 = this.add.image(998, 100, "note2").setScale(0.4);
+    var note4 = this.add.image(1098, 100, "note1").setScale(0.4);
   }
 }
--- Source/Scenes/preLoader.js
+++ Source/Scenes/preLoader.js
@@ -8,5 +8,7 @@ export class preLoad extends Phaser.Scene {constructor(){super("Loader");}
     this.load.image("settingsButton", "https://cdn.jsdelivr.net/gh/IProDurant/js-yaf8nf@IProDurant-patch-1/assets/images/Settingsbutton.png");
     this.load.image("exitButton", "https://cdn.jsdelivr.net/gh/IProDurant/js-yaf8nf@IProDurant-patch-1/assets/images/exitoff.png");
     this.load.image("gameGrid", "https://cdn.jsdelivr.net/gh/IProDurant/unique-game-name@master/Assets/images/maniaGrid.jpg");
+    this.load.image("note1", "https://cdn.jsdelivr.net/gh/IProDurant/unique-game-name@master/Assets/images/mania-note2H.png");
+    this.load.image("note2", "https://cdn.jsdelivr.net/gh/IProDurant/unique-game-name@master/Assets/images/mania-note1H.png");
   }
 }
