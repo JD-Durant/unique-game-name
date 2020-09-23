8a16d06a5ae69edd6a3d979a7b0fbbec22cf1fb9
IProDurant <joshdurant2001@gmail.com>
2020-09-23 10:57:45 +0100

    1 file modified

--- Source/Scenes/Calculations/notePosition.js
+++ Source/Scenes/Calculations/notePosition.js
@@ -1,6 +1,7 @@
 import "phaser";
 import {note1} from '../game'
 export class notePosition extends Phaser.Scene {constructor(){super("notes");}
+
   recordNote(note) {
     var position = note.y;
     console.log(position);
