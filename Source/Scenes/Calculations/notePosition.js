8f35e01464aabe2b70972f1fb27ce8238a931cdf
IProDurant <joshdurant2001@gmail.com>
2020-10-14 10:59:25 +0100

    3 files modified

--- Source/Scenes/Calculations/notePosition.js
+++ Source/Scenes/Calculations/notePosition.js
@@ -1,14 +1,16 @@
 var position;
 import { scoreCalc } from "./scoreCalculator";
+import { changeHealth } from '../game';
 
 export function recordNote(note) {
   if (note.y > 900) {
     note.y = -25;
-    console.log("Miss"); 
+    console.log("Miss");
+    changeHealth(-15);
   };
 };
 
-export function resetNote(note) {note.y = -25;};
+export function resetNote(note) {note.y = -25;}; //called during hit detection in order to reset the note
 
 export function inPosition(note, key) {
   position = note.y;
--- Source/Scenes/game.js
+++ Source/Scenes/game.js
@@ -1,17 +1,20 @@
 var note1, note2, note3, note4;
 var score = 0
 var scoreText
+var health = 100
+var healthText
 export { note1, note2, note3, note4};
 import { song } from './Maps/firstMap'
 import { moveNote } from './Calculations/notePosition'
-var counter = 0;
 
-export function changeScore(scoreChange) { score = scoreChange };
+export function changeScore(scoreChange) { score += scoreChange };
+export function changeHealth(healthChange) { health += healthChange };
 
 export class actualGame extends Phaser.Scene {constructor(){super("game");}
   create(){
     var background =  this.add.image(0, 0, "backGround").setOrigin(0,0).setScale(2);
-    scoreText = this.add.text(10, 0, "Score : " + score, { fontFamily: 'Arial', fontSize: 64, color: 'black' })
+    scoreText = this.add.text(10, 0, "Score : " + score, { fontFamily: 'Verdana', fontSize: 64, color: 'black' })
+    healthText = this.add.text(10, 60, "Health : " + health, { fontFamily: 'Verdana', fontSize: 64, color: 'black' })
     this.scene.launch("keyDetector"); //launches a scene in the background, this is how i will carry out any key detection
     var grid = this.add.image(950, 475, "gameGrid").setScale(0.9);
     note1 = this.add.image(800, -20, "note1").setScale(0.41); //it just works ok.
@@ -26,6 +29,7 @@ export class actualGame extends Phaser.Scene {constructor(){super("game");}
     moveNote(note3, 9)
     moveNote(note4, 7)
     scoreText.setText("Score : " + score)
+    healthText.setText("Health : " + health)
     //song();
   };
 };
