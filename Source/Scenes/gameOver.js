00401766c7f77f4c9e2f5243772afbb01ca2ccff
IProDurant <joshdurant2001@gmail.com>
2020-10-30 19:22:16 +0000

    1 file modified

--- Source/Scenes/gameOver.js
+++ Source/Scenes/gameOver.js
@@ -1,6 +1,6 @@
 export class endGame extends Phaser.Scene {constructor(){super("gameOver");}
 create() {
   var background =  this.add.image(0, 0, "backGround").setOrigin(0,0).setScale(2);
-  var endText = this.add.image(400, 500, "gameover");
+  var endText = this.add.image(450, 300, "gameover");
   }
 }
